0c21e826cc4582f0b6776ae13ccdc8fd3b8b286b
niushuai <niushuai951101@gmail.com>
2021-12-10 11:51:14 +0800

    fix: 修改常量名

--- Utils/UrlConstant.cs
+++ Utils/UrlConstant.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BedWorker.Utils
+﻿namespace BedWorker.Utils
 {
     public class UrlConstant
     {
-        public static string Url_HttpServer = "http://localhost:65501/BedWorker/";
-        public static string Url_Gitee_GetCode = "https://gitee.com/oauth/authorize?client_id={0}&redirect_uri={1}&response_type=code";
-        public static string Url_Gitee_Code2Token = "https://gitee.com/oauth/token?grant_type=authorization_code&code={0}&client_id={1}&redirect_uri={2}&client_secret={3}";
+        public static string HttpServer = "http://localhost:65501/BedWorker/";
+        public static string Gitee_GetCode = "https://gitee.com/oauth/authorize?client_id={0}&redirect_uri={1}&response_type=code";
+        public static string Gitee_Code2Token = "https://gitee.com/oauth/token?grant_type=authorization_code&code={0}&client_id={1}&redirect_uri={2}&client_secret={3}";
+        public static string Gitee_Userinfo = "https://gitee.com/api/v5/user";
     }
 }
