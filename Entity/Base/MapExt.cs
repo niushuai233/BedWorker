1f7b0970cd203634c1311e0cb0b053bf17cfe48f
niushuai <niushuai951101@gmail.com>
2021-12-10 17:30:32 +0800

    feat: 引入map

---
+++ Entity/Base/MapExt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BedWorker.Entity.Base
+{
+    public class MapExt : Dictionary<string, object>
+    {
+        public bool IsNullOrEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void Put(string key, string value)
+        {
+            this.Add(key, value);
+        }
+
+        public object Get(string key)
+        {
+            try
+            {
+                return this[key];
+            } 
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
